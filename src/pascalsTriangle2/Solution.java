44708af6886f7fc74c35ccb2f1ad3407795c2985
Fichardu <xufu90@gmail.com>
2015-01-08 23:25:08 +0800

    Pascals Triangle II

--- src/pascalsTriangle2/Solution.java
+++ src/pascalsTriangle2/Solution.java
@@ -14,4 +14,15 @@ public class Solution {
         }
         return row;
     }
+
+    public List<Integer> getRow2(int rowIndex) {
+        Integer[] row = new Integer[rowIndex+1];
+        for (int i=0; i<=rowIndex; ++i) {
+            row[i] = 1;
+            for (int j=i-1; j>0; --j) {
+                row[j] = row[j] + row[j-1];
+            }
+        }
+        return Arrays.asList(row);
+    }
 }
